7b946e28d915ff461142d0cc67f26a18ac27122f
Nat Silprasert <natsilprasert@gmail.com>
2025-11-12 04:30:35 +0700

    fix my prompts css, grid layout and reponsive nav bar

--- frontend/public/scripts/main.js
+++ frontend/public/scripts/main.js
@@ -1,8 +1,12 @@
 // Toggle menu visibility on mobile
 const hamburger = document.getElementById("hamburger");
+const closeNav = document.getElementById("nav-x");
 const nav = document.getElementById("nav");
 
 hamburger.addEventListener("click", () => {
     nav.classList.toggle("active");
-    hamburger.classList.toggle("active");
+});
+
+closeNav.addEventListener("click", () => {
+    nav.classList.toggle("active");
 });
